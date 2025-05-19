b46a6afe1380387a716b11b57e618df2b0ac43e6
Hurtadx <andreshm253@gmail.com>
2025-05-19 12:24:22 -0500

    Create The first files for the api

---
+++ src/main/java/com/lavaca/lavacaapi/LavacaApiApplication.java
@@ -0,0 +1,13 @@
+package com.lavaca.lavacaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavacaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavacaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lavaca/lavacaapi/LavacaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavaca.lavacaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavacaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
